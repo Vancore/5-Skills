b290ea0e95dd70d176d5e685033d82397ebb3689
Marko Nonninger <Marko.Nonninger@scout24.ch>
2021-07-14 16:52:20 +0200

    Marko - Added click listener

--- AllInOne/app/src/main/java/vancore/all_in_one/five_skills/FiveSkillsActivity.kt
+++ AllInOne/app/src/main/java/vancore/all_in_one/five_skills/FiveSkillsActivity.kt
@@ -1,9 +1,11 @@
 package vancore.all_in_one.five_skills
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.all_in_one.R
@@ -12,7 +14,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class FiveSkillsActivity: AppCompatActivity() {
+class FiveSkillsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFiveSkillsBinding
 
@@ -22,17 +24,23 @@ class FiveSkillsActivity: AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-//        binding.bnvFiveSkills.setOnNavigationItemSelectedListener { item ->
-//            when(item.itemId) {
-//                R.id.skillProfile ->
-//            }
-//        }
-
         val navigationController = findNavController(R.id.host_fragment_five_skills)
         val appBarConfiguration = AppBarConfiguration(setOf(R.id.browserFragment, R.id.skillProfileFragment))
         setupActionBarWithNavController(navigationController, appBarConfiguration)
         binding.bnvFiveSkills.setupWithNavController(navigationController)
 
+        binding.bnvFiveSkills.setOnNavigationItemSelectedListener { item ->
+            // Fun fact: Does not work if you set it before setting it up with NavController etc.
+            if (NavigationUI.onNavDestinationSelected(item, navigationController)) {
+                when (item.itemId) {
+                    R.id.skillProfileFragment -> Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
+                    R.id.browserFragment -> Toast.makeText(this, "Browser clicked", Toast.LENGTH_SHORT).show()
+                }
+                true
+            } else {
+                false
+            }
+        }
     }
 
 }

a4f6c5497cb4b58d28a2730bffad3d1dcd7e1d4f
Marko Nonninger <Marko.Nonninger@scout24.ch>
2021-07-26 14:54:00 +0200

    Marko - Login and Logout without Loading Spinner

--- AllInOne/app/src/main/java/vancore/all_in_one/five_skills/skill_profile/ui/SkillProfileFragment.kt
+++ AllInOne/app/src/main/java/vancore/all_in_one/five_skills/skill_profile/ui/SkillProfileFragment.kt
@@ -8,12 +8,15 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.example.all_in_one.R
 import com.example.all_in_one.databinding.FragmentSkillProfileBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class SkillProfileFragment : Fragment() {
@@ -25,7 +28,7 @@ class SkillProfileFragment : Fragment() {
 
 
     @Inject
-    lateinit var profileViewModel: SkillProfileViewModel
+    lateinit var viewModel: SkillProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,10 +43,21 @@ class SkillProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         auth = Firebase.auth
 
-        profileViewModel.isUserOnline.observe(viewLifecycleOwner, {
-            binding.bLogin.isVisible = !it
-            binding.bRegister.isVisible = !it
-            binding.bLogout.isVisible = it
+        viewModel.isUserOnline.observe(viewLifecycleOwner, { user ->
+            binding.bLogin.isVisible = user == null
+            binding.bRegister.isVisible = user == null
+            binding.bLogout.isVisible = user != null
+
+            if (user != null) {
+                // What is user.displayName?
+                binding.tvLogin.text = getString(R.string.profile_login_logged_in, user.email)
+                binding.tietAccountName.visibility = View.GONE
+                binding.tilPassword.visibility = View.GONE
+            } else {
+                binding.tvLogin.text = getString(R.string.profile_login_logged_out)
+                binding.tietAccountName.visibility = View.VISIBLE
+                binding.tilPassword.visibility = View.VISIBLE
+            }
         })
 
         binding.bLogin.setOnClickListener {
@@ -64,8 +78,8 @@ class SkillProfileFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        profileViewModel.doSomething()
-        profileViewModel.checkIfUserIsOnline(auth)
+        viewModel.doSomething()
+        viewModel.checkIfUserIsOnline(auth)
     }
 
     private fun signIn(email: String, password: String) {
@@ -76,6 +90,7 @@ class SkillProfileFragment : Fragment() {
                     Log.d(tag, "signInWithEmail:success")
                     val user = auth.currentUser
                     // Update from ViewModel
+                    viewModel.onLoginSuccessful(user)
                     //updateUI(user)
                 } else {
                     // If sign in fails, display a message to the user.
@@ -85,6 +100,7 @@ class SkillProfileFragment : Fragment() {
                         Toast.LENGTH_SHORT
                     ).show()
                     // Update from ViewModel
+                    viewModel.onLoginFailed()
                     //updateUI(user)
                 }
             }
@@ -92,6 +108,9 @@ class SkillProfileFragment : Fragment() {
 
     private fun signOut() {
         auth.signOut()
+        auth.addAuthStateListener {
+            if (it.currentUser == null) viewModel.onSignOut()
+        }
     }
 
     private fun createUserWithMailAndPassword(email: String, password: String) {
@@ -102,7 +121,7 @@ class SkillProfileFragment : Fragment() {
                     Log.d("TAG", "createUserWithEmail:success")
                     val user = auth.currentUser
                     // Update from ViewModel
-                    //updateUI(user)
+                    viewModel.onUserCreationSuccessFully(user)
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w("TAG", "createUserWithEmail:failure", task.exception)
@@ -112,6 +131,7 @@ class SkillProfileFragment : Fragment() {
                     ).show()
                     //Update from ViewModel
                     //updateUI(null)
+                    viewModel.onUserCreationFailed()
                 }
             }
     }
--- AllInOne/app/src/main/java/vancore/all_in_one/five_skills/skill_profile/ui/SkillProfileViewModel.kt
+++ AllInOne/app/src/main/java/vancore/all_in_one/five_skills/skill_profile/ui/SkillProfileViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import vancore.all_in_one.five_skills.skill_profile.data.SkillProfileRepository
 import javax.inject.Inject
 
@@ -11,8 +12,10 @@ class SkillProfileViewModel @Inject constructor(
     private val profileRepository: SkillProfileRepository
 ) : ViewModel() {
 
-    private val _isUserOnline: MutableLiveData<Boolean> = MutableLiveData()
-    val isUserOnline: LiveData<Boolean>
+    var currentUser: FirebaseUser? = null
+
+    private val _isUserOnline: MutableLiveData<FirebaseUser> = MutableLiveData()
+    val isUserOnline: LiveData<FirebaseUser>
         get() = _isUserOnline
 
     fun doSomething() {
@@ -22,11 +25,34 @@ class SkillProfileViewModel @Inject constructor(
     fun checkIfUserIsOnline(auth: FirebaseAuth) {
         profileRepository.profileMethod()
         // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
+        currentUser = auth.currentUser
         if (currentUser != null) {
-            _isUserOnline.postValue(true)
+            _isUserOnline.postValue(currentUser)
         } else {
-            _isUserOnline.postValue(false)
+            _isUserOnline.postValue(currentUser)
         }
+    }
+
+    fun onLoginSuccessful(user: FirebaseUser?) {
+        currentUser = user
+        _isUserOnline.postValue(currentUser)
+    }
+
+    fun onLoginFailed() {
+        _isUserOnline.postValue(currentUser)
+    }
+
+    fun onSignOut() {
+        currentUser = null
+        _isUserOnline.postValue(currentUser)
+    }
+
+    fun onUserCreationSuccessFully(user: FirebaseUser?) {
+        currentUser = user
+        _isUserOnline.postValue(currentUser)
+    }
+
+    fun onUserCreationFailed() {
+        //_isUserOnline.postValue()
     }
 }
